db6e1e3ea8700291f3d25882dcd17a1529a6a590
ihwan <aihwan65@gmail.com>
2026-05-21 22:23:50 +0700

    feat : config

--- application/config/config.php
+++ application/config/config.php
@@ -531,5 +531,7 @@ $config['rewrite_short_tags'] = FALSE;
 */
 $config['proxy_ips'] = '';
 
-$config['base_url'] = 'http://web-travel-haji-umroh.test/';
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 $config['index_page'] = '';
